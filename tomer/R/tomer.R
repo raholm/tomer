a83037bb47ee5a0192520877f9afee69a4eb9b8b
raholm <rasmus.holm723@gmail.com>
2017-07-12 16:44:09 +0200

    Init package.

---
+++ tomer/R/tomer.R
@@ -0,0 +1,8 @@
+#' tomer R package
+#'
+#' @description
+#' An R package for evaluating topic models
+#'
+#' @docType package
+#' @name tomer
+NULL
---
+++ tomer/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tomer)
+
+test_check("tomer")
