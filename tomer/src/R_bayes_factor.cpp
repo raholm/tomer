bfb0175691d251c63769c39a95202b7082c7262b
raholm <rasmus.holm723@gmail.com>
2017-08-18 02:10:14 +0200

    R-Cpp interface for BayesFactor.

--- tomer/src/R_bayes_factor.cpp
+++ tomer/src/R_bayes_factor.cpp
@@ -1,13 +1,39 @@
 #include <Rcpp.h>
 
 #include "def.h"
+#include "topic_indicator_mode.h"
 #include "bayes_factor.h"
 
 using namespace tomer;
 
-// [[Rcpp::export]]
-double compute_log_bayes_factor_cpp(const Rcpp::IntegerVector& topic_indicators,
-                                    size_t n_topics) {
-  return compute_log_bayes_factor(Rcpp::as<IntVector>(topic_indicators),
-                                  n_topics);
+void mode_update(TypeTopicIndicatorMode* const mode,
+                 const Rcpp::StringVector& tokens,
+                 const Rcpp::IntegerVector& topic_indicators) {
+  mode->update(Rcpp::as<StringVector>(tokens),
+               Rcpp::as<IntVector>(topic_indicators));
+}
+
+double mode_compute_log_bayes_factor(TypeTopicIndicatorMode* const mode,
+                                     const Rcpp::StringVector& tokens,
+                                     size_t n_topics,
+                                     double beta) {
+  IntVector topic_indicators;
+  topic_indicators.reserve(tokens.size());
+
+  for (const auto& token : tokens) {
+    auto type = Rcpp::as<TypeTopicIndicatorMode::type>(token);
+    if (mode->contains(type))
+      topic_indicators.push_back(mode->get_mode(type));
+  }
+
+  return compute_log_bayes_factor(topic_indicators, n_topics, beta);
+}
+
+RCPP_MODULE(mod_bayes_factor) {
+  Rcpp::class_<TypeTopicIndicatorMode>("TypeTopicIndicatorMode")
+    .constructor()
+
+    .method("update", &mode_update)
+    .method("compute_log_bayes_factor", &mode_compute_log_bayes_factor)
+    ;
 }
