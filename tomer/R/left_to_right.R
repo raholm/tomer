b155317010f10c1b22c195d385be2b89dee5d8bd
raholm <rasmus.holm723@gmail.com>
2017-08-19 19:09:48 +0200

    Updated exports. Fixed so that left-to-right now takes tokens instead and added some internal documentation notes.

--- tomer/R/left_to_right.R
+++ tomer/R/left_to_right.R
@@ -3,21 +3,23 @@
 #' @description This is an algorithm for approximating p(w | ...) blabla
 #'
 #' @export
-evaluate_left_to_right <- function(corpus, state, n_topics, alpha, beta, n_particles, resampling) {
-    checkr::assert_tidy_table(state, c("type", "token", "topic"))
-    checkr::assert_tidy_table(corpus, c("id", "text"))
-    checkr::assert_integer(n_topics, len=1, lower=1)
+evaluate_left_to_right <- function(test_documents, model, ntopics,
+                                   alpha, beta, nparticles, resampling) {
+    checkr::assert_tidy_table(model, c("type", "token", "topic"))
+    checkr::assert_tidy_table(test_documents, c("id", "token"))
+    checkr::assert_integer(ntopics, len=1, lower=1)
+    checkr::assert_integer(nparticles, len=1, lower=1)
     checkr::assert_numeric(beta, len=1, lower=0)
-    checkr::assert_numeric(alpha, len=n_topics, lower=0)
+    checkr::assert_numeric(alpha, len=ntopics, lower=0)
     checkr::assert_logical(resampling, len=1)
 
-    n_docs <- nrow(corpus)
+    ndocs <- length(unique(test_documents$id))
 
-    tokens <- corpus %>%
-        texcur::tf_tokenize()  %>%
-        dplyr::mutate(id=as.numeric(id))
+    ## The following -1s are there to make it
+    ## compatible with C++ (Zero-based numbering
+    ## https://en.wikipedia.org/wiki/Zero-based_numbering)
 
-    alphabet <- state %>%
+    alphabet <- model %>%
         dplyr::group_by(type, token) %>%
         dplyr::filter(row_number() == 1) %>%
         dplyr::ungroup() %>%
@@ -25,27 +27,27 @@ evaluate_left_to_right <- function(corpus, state, n_topics, alpha, beta, n_parti
         dplyr::mutate(type=as.numeric(type) - 1,
                       token=as.character(token))
 
-    topic_counts <- state %>%
+    topic_counts <- model %>%
         dplyr::group_by(topic) %>%
         dplyr::summarise(count=n()) %>%
         dplyr::ungroup() %>%
         dplyr::mutate(topic=as.numeric(topic) - 1)
 
-    type_topic_counts <- state %>%
+    type_topic_counts <- model %>%
         dplyr::group_by(type, topic) %>%
         dplyr::summarise(count=n()) %>%
         dplyr::ungroup() %>%
         dplyr::mutate(type=as.numeric(type) - 1,
                       topic=as.numeric(topic) - 1)
 
-    evaluate_left_to_right_cpp(tokens,
-                               n_docs,
+    evaluate_left_to_right_cpp(test_documents,
+                               ndocs,
                                alphabet,
-                               n_topics,
+                               ntopics,
                                topic_counts,
                                type_topic_counts,
                                alpha,
                                beta,
-                               n_particles,
-                               resampling);
+                               nparticles,
+                               resampling)
 }
