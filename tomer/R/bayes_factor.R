3919caeba3f11b38ba18362ab68b0c36d731ca9c
raholm <rasmus.holm723@gmail.com>
2017-08-21 17:31:40 +0200

    Fixed bug.

--- tomer/R/bayes_factor.R
+++ tomer/R/bayes_factor.R
@@ -8,7 +8,7 @@ log_bayes_factor <- function(topic_indicators, ntopics, beta) {
     compute_log_bayes_factor_cpp(topic_indicators, ntopics, beta)
 }
 
-.check_input_bayes_factor <- function(topic_indicators, mode, ntopics, beta) {
+.check_input_bayes_factor <- function(topic_indicators, ntopics, beta) {
     checkr::assert_integer(topic_indicators, lower=0, upper=ntopics - 1)
     checkr::assert_integer(ntopics, len=1, lower=1)
     checkr::assert_numeric(beta, len=1, lower=0, upper=1)
