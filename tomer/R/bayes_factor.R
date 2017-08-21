66e3a01931adeffad23fb982b39004251e34611f
raholm <rasmus.holm723@gmail.com>
2017-08-21 16:54:36 +0200

    Fixed bayes factor R interface.

--- tomer/R/bayes_factor.R
+++ tomer/R/bayes_factor.R
@@ -1,6 +1,6 @@
 #' @export
-log_bayes_factor <- function(corpus, mode, ntopics, beta) {
-    .check_input_bayes_factor(corpus, mode, ntopics, beta)
+log_bayes_factor <- function(topic_indicators, ntopics, beta) {
+    .check_input_bayes_factor(topic_indicators, ntopics, beta)
 
     ## topic_indicators <- types_to_topic_indicators(corpus$token, mode)
     ## module <- get_bayes_factor_module()
@@ -8,8 +8,8 @@ log_bayes_factor <- function(corpus, mode, ntopics, beta) {
     compute_log_bayes_factor_cpp(topic_indicators, ntopics, beta)
 }
 
-.check_input_bayes_factor <- function(corpus, mode, ntopics, beta) {
-    checkr::assert_tidy_table(corpus, "token")
+.check_input_bayes_factor <- function(topic_indicators, mode, ntopics, beta) {
+    checkr::assert_integer(topic_indicators, lower=0, upper=ntopics - 1)
     checkr::assert_integer(ntopics, len=1, lower=1)
     checkr::assert_numeric(beta, len=1, lower=0, upper=1)
 }
