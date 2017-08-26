eb4aa4b5c0a1903a688f764c31c805dae34b8569
raholm <rasmus.holm723@gmail.com>
2017-08-26 13:42:29 +0200

    Composed the sampling functions into a single function.

--- tomer/R/sampling.R
+++ tomer/R/sampling.R
@@ -1,5 +1,8 @@
 #' @export
 sample_document <- function(size, model, alpha) {
+    checkr::assert_integer(size, lower=1)
+
+    n <- length(size)
     ntopics <- nrow(model)
     nwords <- ncol(model)
 
@@ -8,43 +11,6 @@ sample_document <- function(size, model, alpha) {
 
     if (length(alpha) == 1)
         alpha <- rep(alpha, ntopics)
-
-    topic_probs <- MCMCpack::rdirichlet(1, alpha)
-
-    topic_indicators <- sample(x=1:ntopics,
-                               size=size,
-                               replace=TRUE,
-                               prob=topic_probs)
-
-    sample_word <- function(topic) {
-        word_probs <- model[topic,]
-        sample(x=1:nwords,
-               size=1,
-               replace=TRUE,
-               prob=word_probs)
-    }
-
-    words <- sapply(topic_indicators, sample_word)
-
-    dplyr::data_frame(token=words, topic=topic_indicators)
-}
-
-#' @export
-sample_corpus <- function(n, size, model, alpha) {
-    ntopics <- nrow(model)
-    nwords <- ncol(model)
-
-    if (length(alpha) != 1 & length(alpha) != ntopics)
-        stop("length of alpha has to be either one or the number of topics.")
-
-    if (length(alpha) == 1)
-        alpha <- rep(alpha, ntopics)
-
-    if (length(size) != 1 & length(size) != n)
-        stop("size must be equal length to 1 or n")
-
-    if (length(size) == 1)
-        size <- rep(size, n)
 
     topic_probs <- MCMCpack::rdirichlet(n, alpha)
 
@@ -65,7 +31,7 @@ sample_corpus <- function(n, size, model, alpha) {
 
         })
 
-        dplyr::data_frame(id=rep(id, length(tokens)),
+        dplyr::data_frame(doc=rep(id, length(tokens)),
                           token=tokens,
                           topic=topic_indicators)
     }
