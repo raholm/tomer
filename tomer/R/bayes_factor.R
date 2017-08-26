9ba049ec964060cb06f89c634ea18e1a06fd9258
raholm <rasmus.holm723@gmail.com>
2017-08-26 13:42:55 +0200

    Changed computation of bayes factor to work with topic indicators starting at 1.

--- tomer/R/bayes_factor.R
+++ tomer/R/bayes_factor.R
@@ -2,11 +2,12 @@
 log_bayes_factor <- function(topic_indicators, ntopics, beta) {
     .check_input_bayes_factor(topic_indicators, ntopics, beta)
 
-    compute_log_bayes_factor_cpp(topic_indicators, ntopics, beta)
+    ## -1 to make it compatible with C++
+    compute_log_bayes_factor_cpp(topic_indicators - 1, ntopics, beta)
 }
 
 .check_input_bayes_factor <- function(topic_indicators, ntopics, beta) {
-    checkr::assert_integer(topic_indicators, lower=0, upper=ntopics - 1)
+    checkr::assert_integer(topic_indicators, lower=1, upper=ntopics)
     checkr::assert_integer(ntopics, len=1, lower=1)
     checkr::assert_numeric(beta, len=1, lower=0, upper=1)
 }
