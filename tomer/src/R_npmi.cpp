a2e13075284cc338c054c3ef5c5ea6fde2666bed
raholm <rasmus.holm723@gmail.com>
2017-09-14 16:16:21 +0200

    Implemented a tokenizer and modified npmi-call.

--- tomer/src/R_npmi.cpp
+++ tomer/src/R_npmi.cpp
@@ -4,6 +4,7 @@
 #include "util.h"
 #include "topic_evaluator_util.h"
 #include "topic_evaluator.h"
+#include "tokenizer.h"
 
 using namespace tomer;
 
@@ -12,10 +13,15 @@ Rcpp::NumericVector evaluate_npmi_cpp(const Rcpp::List& topics,
                                       const Rcpp::List& documents,
                                       size_t window_size) {
   Corpus itopics = convert_from_R(topics);
-  Corpus idocuments = convert_from_R(documents);
+  Vector<String> tmp_docs = Rcpp::as<Vector<String>>(documents);
+
+  Tokenizer tokenizer(" ");
+  Corpus docs = tokenizer.transform(tmp_docs);
+
+  // Corpus idocuments = convert_from_R(documents);
 
   TopicEvaluatorData data(std::move(create_word_counts(itopics)));
-  calculate_word_counts_and_window_count(idocuments, window_size, &data);
+  calculate_word_counts_and_window_count(docs, window_size, &data);
 
   NpmiEvaluator evaluator(data.word_counts, data.window_count);
   auto ntopics = itopics.size();
@@ -27,3 +33,25 @@ Rcpp::NumericVector evaluate_npmi_cpp(const Rcpp::List& topics,
 
   return Rcpp::wrap(vals);
 }
+
+
+// // [[Rcpp::export]]
+// Rcpp::NumericVector evaluate_npmi_cpp(const Rcpp::List& topics,
+//                                       const Rcpp::List& documents,
+//                                       size_t window_size) {
+//   Corpus itopics = convert_from_R(topics);
+//   Corpus idocuments = convert_from_R(documents);
+
+//   TopicEvaluatorData data(std::move(create_word_counts(itopics)));
+//   calculate_word_counts_and_window_count(idocuments, window_size, &data);
+
+//   NpmiEvaluator evaluator(data.word_counts, data.window_count);
+//   auto ntopics = itopics.size();
+//   Vector<double> vals;
+//   vals.reserve(ntopics);
+
+//   for (auto const& topic : itopics)
+//     vals.push_back(evaluator.evaluate(topic));
+
+//   return Rcpp::wrap(vals);
+// }
---
+++ tomer/src/tokenizer.h
@@ -0,0 +1,48 @@
+#ifndef TOMER_TOKENIZER_H_
+#define TOMER_TOKENIZER_H_
+
+#include <cstring>
+
+#include "def.h"
+
+namespace tomer {
+
+  class Tokenizer {
+  public:
+    Tokenizer(const String& delimiter)
+      : cache_size_{5000},
+        delimiter_{delimiter} {}
+
+    Corpus transform(const Vector<String>& texts) const {
+      Corpus corpus;
+      corpus.reserve(texts.size());
+      for (auto const& text : texts)
+        corpus.push_back(transform(text));
+      return corpus;
+    }
+
+    Document transform(const String& text) const {
+      Document document;
+      document.reserve(cache_size_);
+
+      char* t = const_cast<char*>(text.c_str());
+      char* current_token = strtok(t, delimiter_.c_str());
+
+      while (current_token != NULL) {
+        document.push_back(String(current_token));
+        current_token = strtok(NULL, delimiter_.c_str());
+      }
+
+      document.shrink_to_fit();
+      return document;
+    }
+
+  private:
+    size_t cache_size_;
+    String delimiter_;
+
+  };
+
+} // namespace tomer
+
+#endif // TOMER_TOKENIZER_H_
