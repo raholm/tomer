bfb296e2a4c8a23030c96955bb893c0689f05ef7
raholm <rasmus.holm723@gmail.com>
2017-09-17 21:55:34 +0200

    Modified tokenizers.

--- tomer/src/tokenizer.h
+++ tomer/src/tokenizer.h
@@ -8,35 +8,56 @@
 
 namespace tomer {
 
+  template<typename T>
   class Tokenizer {
   public:
-    Tokenizer(const String& delimiter)
-      : cache_size_{5000},
-        delimiter_{delimiter} {}
+    using Token = T;
 
-    Corpus transform(const Vector<String>& texts) const {
-      Corpus corpus;
-      corpus.reserve(texts.size());
+    virtual ~Tokenizer() = default;
+
+    virtual Matrix<Token> transform(const StringVector& texts) const {
+      Matrix<Token> tokens;
+      tokens.reserve(texts.size());
+
       for (auto const& text : texts)
-        corpus.push_back(transform(text));
-      return corpus;
+        tokens.push_back(transform(text));
+
+      return tokens;
     }
 
-    Document transform(const String& text) const {
-      Document document;
-      document.reserve(cache_size_);
+    virtual Vector<Token> transform(const String& text) const = 0;
+
+  };
+
+  class WordTokenizer : public Tokenizer<Word> {
+  public:
+    using BaseClass = Tokenizer<Word>;
+    using Token = BaseClass::Token;
+
+    WordTokenizer(const String& delimiter=" ", size_t cache_size=4096)
+      : cache_size_{cache_size},
+        delimiter_{delimiter} {}
+
+    // TODO: Wtf is going on here? Why is this required?
+    Matrix<Token> transform(const StringVector& texts) const override {
+      return BaseClass::transform(texts);
+    }
+
+    Vector<Token> transform(const String& text) const override {
+      Vector<Token> words;
+      words.reserve(cache_size_);
 
       char* t = const_cast<char*>(text.c_str());
       char* current_token = strtok(t, delimiter_.c_str());
 
       while (current_token != NULL) {
-        if (*current_token != '')
-          document.push_back(String(current_token));
+        if (*current_token != ' ')
+          words.push_back(String(current_token));
         current_token = strtok(NULL, delimiter_.c_str());
       }
 
-      document.shrink_to_fit();
-      return document;
+      words.shrink_to_fit();
+      return words;
     }
 
   private:
@@ -45,46 +66,46 @@ namespace tomer {
 
   };
 
-  class TypeTokenizer {
+  class WordIndexTokenizer : public Tokenizer<WordIndex> {
   public:
-    Tokenizer(const String& delimiter)
-      : cache_size_{5000},
+    using BaseClass = Tokenizer<WordIndex>;
+    using Token = BaseClass::Token;
+
+    WordIndexTokenizer(const String& delimiter=" ", size_t cache_size=4096)
+      : cache_size_{cache_size},
         delimiter_{delimiter},
         transformer_{} {}
 
-    TypeMatrix transform(const Vector<String>& texts) const {
-      TypeMatrix corpus;
-      corpus.reserve(texts.size());
-      for (auto const& text : texts)
-        corpus.push_back(transform(text));
-      return corpus;
+    // TODO: Wtf is going on here? Why is this required?
+    Matrix<Token> transform(const StringVector& texts) const override {
+      return BaseClass::transform(texts);
     }
 
-    TypeVector transform(const String& text) const {
-      TypeVector document;
-      document.reserve(cache_size_);
+    Vector<Token> transform(const String& text) const override {
+      Vector<Token> indexes;
+      indexes.reserve(cache_size_);
 
       char* t = const_cast<char*>(text.c_str());
       char* current_token = strtok(t, delimiter_.c_str());
 
       while (current_token != NULL) {
-        if (*current_token != '') {
-          String ct{current_token};
-          transformer_.update(ct);
-          document.push_back(transformer_.transform(ct));
+        if (*current_token != ' ') {
+          Word ct{current_token};
+          Token index = transformer_.update_and_transform(ct);
+          indexes.push_back(index);
         }
 
         current_token = strtok(NULL, delimiter_.c_str());
       }
 
-      document.shrink_to_fit();
-      return document;
+      indexes.shrink_to_fit();
+      return indexes;
     }
 
   private:
     size_t cache_size_;
     String delimiter_;
-    WordToIndexTransformer transformer_;
+    mutable WordToIndexTransformer transformer_;
 
   };
 
