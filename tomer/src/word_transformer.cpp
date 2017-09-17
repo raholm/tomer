3939761332d966305a8672024d583d32a0c5b36d
raholm <rasmus.holm723@gmail.com>
2017-09-17 21:57:48 +0200

    Defined static variables of WordToIndexTransformer-class.

---
+++ tomer/src/word_transformer.cpp
@@ -0,0 +1,8 @@
+#include "word_transformer.h"
+
+namespace tomer {
+
+  const WordIndex WordToIndexTransformer::unobserved_word_index= -1;
+  const Word WordToIndexTransformer::unobserved_word = "<!!unknown!!>";
+
+} // namespace tomer
