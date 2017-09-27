9cbe321bd14fee204dc63ddb038ebd2b888af489
raholm <rasmus.holm723@gmail.com>
2017-09-27 22:00:00 +0200

    Fixed bug.

--- tomer/R/topic_coherence.R
+++ tomer/R/topic_coherence.R
@@ -4,7 +4,7 @@ topic_coherence <- function(topics, documents, cache=FALSE, cache_dst=NULL) {
 
     if (cache) {
         checkr::assert_string(cache_dst)
-        evaluate_topic_coherence_with_cache_cpp(topics, documents, window_size, cache_dst)
+        evaluate_topic_coherence_with_cache_cpp(topics, documents, cache_dst)
     } else {
         evaluate_topic_coherence_cpp(topics, documents, window_size)
     }
