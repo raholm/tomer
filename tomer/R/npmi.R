50bd7559ce3b2b1c6aa8fc3bdea0ef609cd9661a
raholm <rasmus.holm723@gmail.com>
2017-09-28 21:32:25 +0200

    Updated npmi/tc document checks to only apply when required.

--- tomer/R/npmi.R
+++ tomer/R/npmi.R
@@ -1,8 +1,11 @@
 #' @export
 npmi <- function(topics, documents, window_size, cache=FALSE, cache_dst=NULL) {
-    stopifnot(is.character(topics) & is.character(documents))
+    stopifnot(is.character(topics))
     checkr::assert_integer(window_size, len=1, lower=0)
     checkr::assert_logical(cache, len=1)
+
+    if (!cache || (cache && !file.exists(cache_dist)))
+        stopifnot(is.character(documents))
 
     if (cache) {
         checkr::assert_string(cache_dst)
--- tomer/R/topic_coherence.R
+++ tomer/R/topic_coherence.R
@@ -1,6 +1,9 @@
 #' @export
 topic_coherence <- function(topics, documents, cache=FALSE, cache_dst=NULL) {
-    stopifnot(is.character(topics) & is.character(documents))
+    stopifnot(is.character(topics))
+
+    if (!cache || (cache && !file.exists(cache_dist)))
+        stopifnot(is.character(documents))
 
     if (cache) {
         checkr::assert_string(cache_dst)
