9a899731a8a578054bfdadb3cdb1defd67ec5d94
raholm <rasmus.holm723@gmail.com>
2017-10-06 23:42:37 +0200

    Implemented word counts calculation for sparse topic evaluators.

--- tomer/src/topic_evaluator_util.cpp
+++ tomer/src/topic_evaluator_util.cpp
@@ -47,70 +47,58 @@ namespace tomer {
                                               size_t window_size,
                                               const FixedWordIndexTokenizer& tokenizer,
                                               SparseWordIndexTopicEvaluatorData* data) {
-    std::ifstream file{filename};
     String document;
     size_t document_size;
-    WordIndex current_word_index, other_word_index;
     Vector<WordIndex> current_word_indexes;
 
     WordIndexWindow current_word_window(window_size);
     size_t nwindows, head_id, tail_id, nwords;
 
-
     auto& word_index_counts = data->word_index_counts;
     auto& window_count = data->window_count;
 
+    std::ifstream file{filename};
+
     if (file.fail()) {
-      throw new std::runtime_error("Reference file could not be opened.");
+      throw std::runtime_error("Reference file could not be opened.");
     }
 
     while (std::getline(file, document)) {
       current_word_indexes = tokenizer.transform(document);
       document_size = current_word_indexes.size();
 
-      if (window_size == INF_WORD_WINDOW) {
-        nwords = document_size;
-        remove_duplicates_inplace(&current_word_indexes, &nwords);
-
-        for (unsigned current_idx = 0; current_idx < nwords; ++current_idx) {
-          current_word_index = current_word_indexes.at(current_idx);
-
-          if (is_valid_index(current_word_index)) {
-            for (unsigned other_idx = 0; other_idx < nwords; ++other_idx) {
-              other_word_index = current_word_indexes.at(other_idx);
-
-              if (is_valid_index(other_word_index))
-                word_index_counts.update(current_word_index, other_word_index);
-            }
-          }
-        }
-
-        ++window_count;
-      } else {
+      if (window_size == INF_WORD_WINDOW)
+        nwindows = 1;
+      else
         nwindows = document_size + window_size - 1;
 
-        for (size_t j = 1; j < (nwindows + 1); ++j) {
+      for (unsigned j = 1; j < (nwindows + 1); ++j) {
+        if (window_size == INF_WORD_WINDOW) {
+          current_word_window = current_word_indexes;
+          remove_duplicates(&current_word_window);
+          nwords = current_word_window.size();
+        } else {
           head_id = (j > window_size) ? j - window_size : 0;
-          tail_id = std::min(j, document_size);
+          tail_id = std::min((size_t) j, document_size);
           nwords = tail_id - head_id;
 
           for (unsigned i = 0; i < nwords; ++i)
             current_word_window.at(i) = current_word_indexes.at(head_id + i);
 
           remove_duplicates_inplace(&current_word_window, &nwords);
+        }
 
-          for (unsigned left_idx = 0; left_idx < nwords; ++left_idx) {
-            auto left_word_index = current_word_window.at(left_idx);
+        for (unsigned left_idx = 0; left_idx < nwords; ++left_idx) {
+          auto left_word_index = current_word_window.at(left_idx);
 
-            if (is_valid_index(left_word_index)) {
-              word_index_counts.update(left_word_index);
+          if (is_valid_index(left_word_index)) {
+            word_index_counts.update(left_word_index);
 
-              for (unsigned right_idx = left_idx + 1; right_idx < nwords; ++right_idx) {
-                auto right_word_index = current_word_window.at(right_idx);
+            for (unsigned right_idx = left_idx + 1; right_idx < nwords; ++right_idx) {
+              auto right_word_index = current_word_window.at(right_idx);
 
-                if (is_valid_index(right_word_index))
-                  word_index_counts.update(left_word_index, right_word_index);
-              }
+              if (is_valid_index(right_word_index))
+                word_index_counts.update(left_word_index, right_word_index);
             }
           }
         }
